db5230effac2ddc720336685d2d8123947bcf8dd
Jakhongir AV <jakhongir.av@gmail.com>
2023-02-03 01:33:31 +0500

    changing the code structure

--- js/main.js
+++ js/main.js
@@ -4,37 +4,35 @@ const score = document.querySelector(".score");
 const message = document.querySelector(".message");
 const body = document.querySelector(".body");
 
-let changedScore = 20;
 
-const randomNum = Math.trunc(Math.random() * 20);
+const secretNum = Math.trunc(Math.random() * 20);
+let currentScore = 20;
+let attemptsLeft = 20;
+let highScore = 20;
 
-score.innerHTML = changedScore;
 
 
+// console.log(randomNum);
 
-console.log(randomNum);
+btn.addEventListener("click", () => {
+    if (attemptsLeft > 0) {
+        attemptsLeft--;
+        score.innerHTML = attemptsLeft;
 
-btn.addEventListener("click", (a) => {
-    a.preventDefault();
-    const result = +val.value;
-    console.log(result);
-    
-    for (let i = 20; i > 0; i--) {
-        if(+result) {
-                if(result === randomNum) {
-                    message.innerHTML = "True";
-                    body.classList.add("bg_color");
+        let num = Number(val.value);
 
-                }else{
-                    score.innerHTML = changedScore-1;
-                    message.innerHTML = "Wrong!";
-                    body.classList.add("bg_wrong")
-                };
-            }else{
-                alert("It is not a number, Please type a number!")
+        if(num === 0) {
+            message.innerHTML = "No number or 0 is entered";
+        };
+
+        if(num === secretNum) {
+            message.innerHTML = "You won, and you are the best!";
         }
+
+
+
+
+
     }
-
-    
-
 });
+
